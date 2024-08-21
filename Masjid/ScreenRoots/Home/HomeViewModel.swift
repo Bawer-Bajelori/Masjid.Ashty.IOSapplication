5be1a3d294814df5e5fc99d6b11deba6cfbf254b
Lawand-11 <piromarilawand@gmail.com>
2024-08-20 21:43:52 -0700

    Redid homeViewModel and homeView to follow better practices

--- Masjid/ScreenRoots/Home/HomeViewModel.swift
+++ Masjid/ScreenRoots/Home/HomeViewModel.swift
@@ -6,90 +6,118 @@
 //
 
 import Foundation
-struct PrayerTime: Identifiable {
-    let id = UUID()
-    
-    let name : String
-    let prayerTime: String
-}
-enum LoadingState {
-    case loading
-    case error(String)
-    case success([PrayerTime])
-    
-    var prayerTimes: [PrayerTime]? {
-        switch self {
-        case .success(let prayerTimes):
-            return prayerTimes
-        default:
-            return nil
-        }
-    }
-    
-    var errorMessage: String? {
-        switch self {
-        case .error(let message):
-            return message
-        default:
-            return nil
-        }
+
+struct HomeViewState {
+    let prayerTimes: [PrayerTime]
+    let loadingState: LoadingState
+    let errorMessage: String
+    let errorButtonMessage: String
+    let khutbah1Time: String
+    let khutbah2Time: String
+    let khutbahCardTitle: String
+    let prayerColumnTitle: String
+    let athanColumnTitle: String
+    let iqamaColumnTitle: String
+
+    init(
+        prayerTimes: [PrayerTime] = [],
+        loadingState: LoadingState = .loading,
+        errorMessage: String = HOME_ERROR_MESSAGE,
+        errorButtonMessage: String = HOME_ERROR_BUTTON_MESSAGE,
+        khutbah1Time: String = "",
+        khutbah2Time: String = "",
+        khutbahCardTitle: String = KHUTBAH_CARD_TITLE,
+        prayerColumnTitle: String = PRAYER_COLUMN_TITLE,
+        athanColumnTitle: String = ATHAN_COLUMN_TITLE,
+        iqamaColumnTitle: String = IQAMA_COLUMN_TITLE
+    ) {
+        self.prayerTimes = prayerTimes
+        self.loadingState = loadingState
+        self.errorMessage = errorMessage
+        self.errorButtonMessage = errorButtonMessage
+        self.khutbah1Time = khutbah1Time
+        self.khutbah2Time = khutbah2Time
+        self.khutbahCardTitle = khutbahCardTitle
+        self.prayerColumnTitle = prayerColumnTitle
+        self.athanColumnTitle = athanColumnTitle
+        self.iqamaColumnTitle = iqamaColumnTitle
     }
 }
 
+struct PrayerTime {
+    let type: PrayerType
+    let prayerTime: String
+    let iqamaTime: String?
+}
+
+enum PrayerType: String {
+    case fajr = "Fajr"
+    case sunrise = "Sunrise"
+    case dhuhr = "Dhuhr"
+    case asr = "Asr"
+    case maghrib = "Maghrib"
+    case isha = "Isha'a"
+    case khutbah1 = "Arabic/English"
+    case khutbah2 = "Kurdish/English"
+    
+    var prayerName: String {
+        return self.rawValue
+    }
+    /* to use, you could do: let prayer = PrayerType.Fajr
+       then print(prayer.prayername) would give "Fajr"
+    */
+}
+
+enum LoadingState {
+    case loading
+    case error
+    case success
+}
+
 class HomeViewModel: ObservableObject {
-    @Published var loadingState = LoadingState.loading
+    @Published var state: HomeViewState
+    
+    init(state: HomeViewState = HomeViewState()) {
+        self.state = state
+        fetchPrayerData()
+    }
     
     func errorButtonOnClick() {
         fetchPrayerData()
     }
     
-     
-
-     func fetchPrayerData() {
-        API.Client.shared.get(.prayerTime(city: "El Cajon", state: "California", country: "US", method: "2", iso8601: "false")) {(result: Result<API.Types.Response.PrayerTimes, API.Types.Error>) in
+    private func fetchPrayerData() {
+        API.Client.shared.get(.prayerTime(
+            city: CITY_PARAM,
+            state: STATE_PARAM,
+            country: COUNTRY_PARAM,
+            method: METHOD_PARAM,
+            iso8601: ISO8601_PARAM
+        )) { [weak self] (result: Result<API.Types.Response.PrayerTimes, API.Types.Error>) in
             DispatchQueue.main.async {
                 switch result{
                 case .success(let success):
-                    self.loadingState = LoadingState.success(
-                        [
-                            PrayerTime(name: "Fajr", prayerTime: success.data.timings.Fajr),
-                            PrayerTime(name: "Sunrise", prayerTime: success.data.timings.Sunrise),
-                            PrayerTime(name: "Dhuhr", prayerTime: success.data.timings.Dhuhr),
-                            PrayerTime(name: "Asr", prayerTime: success.data.timings.Asr),
-                            PrayerTime(name: "Maghrib", prayerTime: success.data.timings.Maghrib),
-                            PrayerTime(name: "Isha", prayerTime: success.data.timings.Isha)
-                        ])
-                            
-                    print("bawer \(success.data.timings.Fajr)")
+                    let prayerTimes = [
+                        PrayerTime(type: .fajr, prayerTime: success.data.timings.Fajr, iqamaTime: nil),
+                        PrayerTime(type: .sunrise, prayerTime: success.data.timings.Sunrise, iqamaTime: nil),
+                        PrayerTime(type: .dhuhr, prayerTime: success.data.timings.Dhuhr, iqamaTime: nil),
+                        PrayerTime(type: .asr, prayerTime: success.data.timings.Asr, iqamaTime: nil),
+                        PrayerTime(type: .maghrib, prayerTime: success.data.timings.Maghrib, iqamaTime: nil),
+                        PrayerTime(type: .isha, prayerTime: success.data.timings.Isha, iqamaTime: nil)
+                    ]
                     
-                    //self.objectWillChange.send()
-                case .failure(let failure):
-                    self.loadingState = LoadingState.error(failure.errorDescription!)
+                    self?.state = HomeViewState(
+                        prayerTimes: prayerTimes,
+                        loadingState: LoadingState.success
+                    )
+                    
+                case .failure(_):
+                    self?.state = HomeViewState(
+                        prayerTimes: [],
+                        loadingState: LoadingState.error
+                    )
                 }
-            
             }
-            
         }
     }
-    
-    func provideFactory() -> HomeViewModel {
-        return HomeViewModel()
-    }
-}
-
-struct PrayerTimeResponse: Decodable {
-    let data: PrayerTimeData
-}
-
-struct PrayerTimeData: Decodable {
-    let timings: PrayerTimeTimings
-}
-
-struct PrayerTimeTimings: Decodable {
-    let fajr: String
-    let sunrise: String
-    let dhuhr: String
-    let asr: String
-    let maghrib: String
-    let isha: String
 }
--- Masjid/SharedViews/ErrorMessageScreen.swift
+++ Masjid/SharedViews/ErrorMessageScreen.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct ErrorMessageScreen: View {
+    var text: String
+    var buttonText: String
+    var onClick: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            VStack(spacing: 16) {
+                Text(text)
+                    .textTitle()
+                
+                Button(action: onClick) {
+                    Text(buttonText)
+                        .textTitle()
+                        .padding()
+                        .background(Color(CustomColor.PrimaryColor!))
+                        .cornerRadius(5)
+                }
+                .buttonStyle(PlainButtonStyle())
+                
+            }
+            .padding()
+            .background(Color(CustomColor.BackgroundColor!))
+            .cornerRadius(10)
+            .shadow(radius: 10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color(CustomColor.PrimaryColor!), lineWidth: 1.5)
+            )
+            .padding()
+            Spacer()
+        }
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ErrorMessageScreen()
+    ErrorMessageScreen(
+        text: HOME_ERROR_MESSAGE,
+        buttonText: HOME_ERROR_BUTTON_MESSAGE,
+        onClick: {}
+    )
 }
